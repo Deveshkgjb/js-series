b7125b967729a7694a580a7df8d6bf9a2f97fc9f
Deveshkgjb <163760017+Deveshkgjb@users.noreply.github.com>
2025-02-14 20:24:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("hello world")
+
+// var can be re diclare and re-asign
+var num=20;
+num=30;
+var num=30;
+console.log(num);
+
+
+// let can be re-declare but not re-asign 
+let num1=50;
+// let num1=55;
+console.log(num1)
+
+// const can not re-declare not the re-asign
+const num2=44;
+//  const num2=2;
+console.log(num2);
