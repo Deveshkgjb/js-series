b43bb22d5b6ba0d27b92901f972189a6c6057e8a
Deveshkgjb <163760017+Deveshkgjb@users.noreply.github.com>
2025-02-17 22:21:18 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,63 @@
+console.log("hello world");
+
+
+// Hoisting fist assign the variable or funtion
+// only var condition is not follow the hoisting 
+
+num=20;
+console.log(num);
+var num;
+
+let num1; 
+num1 =30;
+console.log(num1);
+
+const num3=80;
+console.log(num3)
+
+// operators
+// + operator
+let a=20;
+let b=40;
+let res=a+b;
+console.log(res);
+
+// + operator
+let c=20;
+let d=40;
+let rese=a%b;
+console.log(rese);
+  
+// == operator
+let e="20";
+// let f=40;
+let f=true;
+let re=e===f;
+console.log(re);
+
+let g="20";
+
+let h=40;
+let r= g!=f;
+console.log(r);
+
+
+let i=20;
+let j=100;
+let k= i>=j;
+console.log(k);
+
+let x=30;
+x=+33;
+console.log(x)
+
+let y=40;
+y+=3;
+console.log(y);
+
+
+var z=45678;
+var stDigit=String(z)[0];
+console.log("the first digit is:" , stDigit);
+
+
